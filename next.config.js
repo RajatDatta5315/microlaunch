6dc54b470350ecf72b75ed006ec76f197551b9c0
RajatDatta5315 <rajatdatta90000@gmail.com>
2026-02-12 09:32:54 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: 'https://microlaunch-api.rajatdatta90000.workers.dev'
-  },
-  output: 'standalone'
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://microlaunch-api.rajatdatta90000.workers.dev'
+  }
 }
+
+module.exports = nextConfig
